c549c43e930dbfa428a9ec423525b0c37eaf5079
Ambika <shekhawatambika1999@gmail.com>
2025-01-28 18:22:52 +0530

    category of a Person

---
+++ src/AgeOfPerson.java
@@ -0,0 +1,20 @@
+public class AgeOfPerson {
+    public static void main(String[] args) {
+        AgeOfPerson age = new AgeOfPerson();
+        age.category(15);
+        age.category(34);
+        age.category(87);
+    }
+    public void category(int age) {
+        if (age < 18) {
+            System.out.println("You are a Minor.");
+        }
+        else if (age >= 18 && age < 60) {
+            System.out.println("You are a Adult.");
+        }
+        else {
+            System.out.println("You are a Senior Citizen.");
+        }
+    }
+}
+    
