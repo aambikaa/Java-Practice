3a5f3b2e28fab077ec8318409ec82cdf988d2f30
Ambika <shekhawatambika1999@gmail.com>
2025-01-28 14:58:00 +0530

    Addition of Numbers

--- src/Addition.java
+++ src/Addition.java
@@ -4,12 +4,9 @@ public class Addition {
     Addition addition = new Addition(); 
     int x = addition.add(6, 4);
     System.out.println("Value of x is "+x);
-
     System.out.println(addition.add(6, 0, 9));
-
     addition.add(3, 3, 60, 3);
     }
-
     public int add(int a, int b) {
         return a+b;    
     }
